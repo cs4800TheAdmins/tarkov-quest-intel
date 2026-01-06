c630f2a9ce2ce5622c8b343cf487792651d9be3b
yz <youssef.zerroug1@gmail.com>
2026-01-06 03:07:51 -0800

    Map asset & domain types

---
+++ src/domain/types.ts
@@ -0,0 +1,39 @@
+export type Tag = {
+    id: string;
+    label: string;
+}
+
+export type Marker = {
+    id: string;
+    name: string;
+    description: string;
+    x: number;
+    y: number;
+    isApproximate: boolean;
+    approximationNote?: string;
+    isVisible: boolean;
+    tags: Tag[];
+}
+
+export type MarkerType = "QuestItem" | "Landmark" | "Extraction" | "BossSpawn" | "KeyLocation" | "Other";
+
+export type GameMap = {
+    id: string;
+    name: string;
+    gameVersion: string;
+    imageUrl: string;
+    widthPx: number;
+    heightPx: number;
+    boundsMinX: number;
+    boundsMinY: number;
+    boundsMaxX: number;
+    boundsMaxY: number;
+}
+
+export type MapState = {
+    centerX: number;
+    centerY: number;
+    zoomLevel: number;
+    minZoom: number;
+    maxZoom: number;
+}
